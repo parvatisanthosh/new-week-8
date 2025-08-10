29f14413e0dd5d494b5abd95ad555aacee6c29c5
parvatisanthosh <parvathygsanthosh@gmail.com>
2025-08-10 16:48:15 +0530

    added an async fucntion to wait to connect to the db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const { userRouter  } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
@@ -6,4 +7,11 @@ const { courseRouter } = require("./routes/course");
 app.use("./user",userRouter);
 app.use("./course",courseRouter);
 
-app.listen(3000);
+
+
+async function main()
+{
+    await mongoose.connect("mongodb+srv://parvati:bqzW04YnHH4RgDbm@cluster0.h3y0kg4.mongodb.net/cousera-database");
+    app.listen(3000);
+    console.log("listening to port 3000");
+}
